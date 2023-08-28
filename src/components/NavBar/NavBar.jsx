b8c71d45916515e42755279df477289263d6c8df
Patricia Riga <patoriga51@gmail.com>
2023-08-28 11:51:25 -0300

    Agregando productos

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,10 +7,9 @@ const NavBar = () => {
             <h1 className={classes.bienvenida}>Productos Ecoargento</h1>
             <section>
                 <button className={classes.boton}>Billeteras</button>
-                <button className={classes.boton}>Organizadores</button>
                 <button className={classes.boton}>Accesorios</button>
-                <button className={classes.boton}>Mascoteras</button>
-                <button className={classes.boton}>Posavasos</button>
+                <button className={classes.boton}>Para el hogar</button>
+                <button className={classes.boton}>Urbanos</button>
                 <CartWidget/>
             </section>
         </nav>
